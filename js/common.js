c9498ca993c8ae1c1d87fb5ed940c7f07d1b7cbb
Echo009 <ech0.extreme@foxmail.com>
2018-04-24 01:49:02 +0800

    申请店铺模块，以及图片上传模块

--- js/common.js
+++ js/common.js
@@ -35,7 +35,8 @@ function simpleWarningInfo(content) {
 var instance = axios.create({
     baseURL: 'http://localhost/book-store/',
     timeout: 500,
-    withCredentials:true
+    withCredentials:true,
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
 });
 function logout() {
     instance.get('/common/logout')
@@ -50,3 +51,24 @@ function logout() {
     //refresh page
     setTimeout("window.location.href='index.html'",MIDDLE_DELAY);
 }
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return false;
+}
+function updateUserInfo() {
+
+    instance.get('/common/userInfo')
+        .then(function (response) {
+            console.log("update userInfo :" + response.data.data.userName);
+            store.set(CURRENT_USER,response.data.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
